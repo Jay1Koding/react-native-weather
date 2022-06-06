53d727e108b080576630e8d75993177ec82b07b9
jay1coding <jay1coding@gmail.com>
2022-06-06 16:44:06 +0900

    React Native Packages/ Third Party Packages

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
+      <Text>Hello World!</Text>
       <StatusBar style='auto' />
     </View>
   );
@@ -16,12 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    fontSize: 29,
-    color: 'red',
   },
 });
--- readme.md
+++ readme.md
@@ -17,3 +17,15 @@
 - 기본적으로 개발자들에게 최대한 많은 APIs와 Components를 제공하려 했음
 - 유지 관리와 업데이트의 어려움을 깨달음
 - 서비스의 규모를 줄이고 Community packages를 써야함
+
+### Component VS APIs
+
+- Component는 화면에 렌더링할 항목 //
+  - ex ) View
+- API는 자바스크립트 코드 // 운영 체제와 소통하는 것
+  - Vibration
+- Expo는 RN의 일부 Component와 APIs를 복제하고 개선함
+  - ex ) 기능은 거의 같지만 함수명이 다름
+  - 대부분의 필요 기능들은 구현해놓음
+
+[Expo SDK](https://docs.expo.dev/versions/latest/)
